fc14b17729aa8f3cf20ef23671185c84489d5de5
Landon-Robert <159073826+Landon-Robert@users.noreply.github.com>
2024-05-31 13:09:59 -0700

    added all 6

--- soundboard.py
+++ soundboard.py
@@ -10,28 +10,39 @@ def playsound2():
 def playsound3():
     playsound("gun-load.mp3")
 def playsound4():
-    playsound("plants-vs-zombies-victory-jingle.mp3")
+    playsound("taco-bell-bong-sfx.mp3")
 def playsound5():
-    playsound("plants-vs-zombies-victory-jingle.mp3")
+    playsound("enter-painting.mp3")
+def playsound6():
+    playsound("among-us-start.mp3")
 
 
 window = tk.Tk()
-window.geometry("200x300")
+window.geometry("280x120")
 l1 = tk.Label(window,text="PVZ Victory riff")
 l2 = tk.Label(window,text="Vine Boom")
 l3 = tk.Label(window,text="Gun Load")
+l4 = tk.Label(window,text="Taco Bell")
+l5 = tk.Label(window,text="Mario 64 painting")
+l6 = tk.Label(window,text="Among us start")
 b1 = tk.Button(window,text="Click to play",command=playsound1)
 b2 = tk.Button(window,text="Click to play",command=playsound2)
 b3 = tk.Button(window,text="Click to play",command=playsound3)
 b4 = tk.Button(window,text="Click to play",command=playsound4)
 b5 = tk.Button(window,text="Click to play",command=playsound5)
+b6 = tk.Button(window,text="Click to play",command=playsound6)
 
-l1.pack()
-b1.pack()
-l2.pack()
-b2.pack()
-l3.pack()
-b3.pack()
-
+l1.grid(row=1,column=1)
+b1.grid(row=2,column=1)
+l2.grid(row=1,column=2)
+b2.grid(row=2,column=2)
+l3.grid(row=1,column=3)
+b3.grid(row=2,column=3)
+l4.grid(row=3,column=1)
+b4.grid(row=4,column=1)
+l5.grid(row=3,column=2)
+b5.grid(row=4,column=2)
+l6.grid(row=3,column=3)
+b6.grid(row=4,column=3)
 
 window.mainloop()
